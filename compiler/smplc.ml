83762aba16757b447bc3c01c909f808882bb1650
dt2450 <dt2450@columbia.edu>
2013-11-22 19:09:46 -0500

    Improved smpl
    Now takes code input from terminal as well

--- compiler/smplc.ml
+++ compiler/smplc.ml
@@ -1,17 +1,6 @@
 type action = DumpAst | CodeGen
 
-let _ =
-    if Array.length Sys.argv <> 3 then (
-        let result = "NAME: \n\tsmpl - Compiler for generating C code for SiMPLe" ^
-        " Programming Language\nARGUMENTS:\n\t -a <code-file> Print the parse" ^
-        " results of the SMPL code in <code-file>\n\t -c <code-file> Generate the C" ^
-        " code equivalent to the SMPL code in <code-file>\nAUTHORS:\n\tWritten" ^
-        " by Ajay Siva Santosh, Andrei Papancea, Devashi Tandon\n\n" in
-        print_string result
-    ) else (
-        let action = List.assoc Sys.argv.(1) [ ("-a", DumpAst); ("-c", CodeGen)] in
-    let src_file = Sys.argv.(2) in  
-    let lexbuf = Lexing.from_channel (open_in src_file) in
+let process_program lexbuf action = 
     let expr = Parser.program Scanner.token lexbuf in
     if (Semantic_checker.validate_program expr = ref true) then
         match action with
@@ -21,5 +10,27 @@ let _ =
         | CodeGen -> 
             let result = Compile.generate_code expr in
             print_string result
-                      
+
+let _ =
+    if Array.length Sys.argv <> 3 then (
+        let result = "NAME: \n\tsmpl - Compiler for generating C code for SiMPLe" ^
+        " Programming Language\nARGUMENTS:\n\t -a code-input Print the parse" ^
+        " results of the input SMPL code \n\t -c code-input Generate the C" ^
+        " code equivalent to the input SMPL code\n code-input can be one of the" ^
+        " following:\n\t -t Take input from terminal\n\t <file-name> Take input" ^
+        " from file\nEXAMPLES:\n\t./smplc -a -t : Take code input from" ^
+        " terminal and print the parse results.\n\t./smplc -c hello.smpl >" ^
+        " hello.c: Compile the SMPL code in hello.smpl and write the generated C" ^ 
+        " code to hello.c\n\nAUTHORS:\n\tWritten" ^
+        " by Ajay Siva Santosh, Andrei Papancea, Devashi Tandon\n\n" in
+        print_string result
+    ) else (
+        let action = List.assoc Sys.argv.(1) [ ("-a", DumpAst); ("-c", CodeGen)] in
+    let src_file = Sys.argv.(2) in  
+    if(src_file = "-t") then
+        let lexbuf = Lexing.from_channel stdin in
+        process_program lexbuf action
+    else
+        let lexbuf = Lexing.from_channel (open_in src_file) in 
+        process_program lexbuf action
     )
